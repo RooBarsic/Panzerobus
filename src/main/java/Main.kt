1e8fb656275f6c6ae140f191c1842b1ee434fca2
Farrukh Karimov <Karimov.farrux@list.ru>
2020-04-05 20:06:16 +0300

    copy repository https://github.com/tseylerd/internship2020-test

---
+++ src/main/java/Main.kt
@@ -0,0 +1,36 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+
+
+data class Point(val latitude: Float, val longitude: Float)
+data class Participants(val passengers: Collection<Person>, val drivers: Collection<Person>)
+data class Person(val id: UUID, val finishPoint: Point)
+
+fun main() {
+    val (passengers, drivers) = readPoints()
+    for (passenger in passengers) {
+        val suggestedDrivers = suggestDrivers(passenger, drivers)
+        println("Passenger point: ${passenger.finishPoint.latitude}, ${passenger.finishPoint.longitude}")
+        for (driver in suggestedDrivers) {
+            println("  ${driver.finishPoint.latitude}, ${driver.finishPoint.longitude}")
+        }
+    }
+}
+
+fun suggestDrivers(passenger: Person, drivers: Collection<Person>): Collection<Person> {
+    TODO("Implement me")
+}
+
+private fun readPoints(): Participants {
+    val pathToResource = Paths.get(Point::class.java.getResource("latlons").toURI())
+    val allPoints = Files.readAllLines(pathToResource).map { asPoint(it) }.shuffled()
+    val passengers = allPoints.slice(0..9).map { Person(UUID.randomUUID(), it) }
+    val drivers = allPoints.slice(10..19).map { Person(UUID.randomUUID(), it) }
+    return Participants(passengers, drivers)
+}
+
+private fun asPoint(it: String): Point {
+    val (lat, lon) = it.split(", ")
+    return Point(lat.toFloat(), lon.toFloat())
+}
