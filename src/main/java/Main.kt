8fa2af463bdc0d0e965c053135aec508b135f227
Farrukh Karimov <Karimov.farrux@list.ru>
2020-04-05 20:56:33 +0300

    Implement-suggestDrivers Добавил функицию оценки удобства водителя.

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,11 +1,13 @@
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
+import kotlin.math.sqrt
 
 
 data class Point(val latitude: Float, val longitude: Float)
 data class Participants(val passengers: Collection<Person>, val drivers: Collection<Person>)
 data class Person(val id: UUID, val finishPoint: Point)
+val startPoint = Point(0F,0F)
 
 fun main() {
     val (passengers, drivers) = readPoints()
@@ -18,8 +20,26 @@ fun main() {
     }
 }
 
+fun countDistance(point1: Point, point2: Point): Float{
+    return sqrt(((point1.latitude - point2.latitude) * (point1.latitude - point2.latitude)) +
+            ((point1.longitude - point2.longitude) * (point1.longitude - point2.longitude)))
+}
+
+fun getDriverComfort(passenger: Person, driver: Person): Float {
+    return countDistance(startPoint, passenger.finishPoint) +
+            countDistance(passenger.finishPoint, driver.finishPoint) -
+            countDistance(startPoint, driver.finishPoint)
+}
+
 fun suggestDrivers(passenger: Person, drivers: Collection<Person>): Collection<Person> {
-    TODO("Implement me")
+    val comparator = Comparator<Person> {driver1: Person, driver2: Person ->
+        when {
+            getDriverComfort(passenger, driver1) > getDriverComfort(passenger, driver2) -> 1
+            getDriverComfort(passenger, driver1) < getDriverComfort(passenger, driver2) -> -1
+            else -> 0
+        }
+    }
+    return drivers.sortedWith(comparator)
 }
 
 private fun readPoints(): Participants {
